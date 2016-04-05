78464e64ba9247b911dc7f9b3d7fff15baa23b77
totemkevin <totemkevin830108@gmail.com>
2016-04-05 23:15:25 +0800

    4/5
    4/5

---
+++ src/NodeChr.java
@@ -0,0 +1,31 @@
+
+public class NodeChr {
+	private int[] chr;
+	private int lengthOfChr;
+	private int adapter;
+	
+	public void setChr(int[] chr)
+	{
+		this.chr=chr;
+		lengthOfChr=chr.length;
+	}
+	public void setAdapter()
+	{
+		this.adapter=adapter;
+	}
+	
+	
+	public int[] getChr()
+	{
+		return chr;
+	}
+	public int getLength()
+	{
+		return lengthOfChr;
+	}
+	public int getAdapter()
+	{
+		return adapter;
+	}
+	
+}
