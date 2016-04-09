fc0d75e3bcef7dcd655fc1223ca930d5ca56750f
totemkevin <totemkevin830108@gmail.com>
2016-04-10 00:45:44 +0800

    4/10
    4/10

--- src/NodeChr.java
+++ src/NodeChr.java
@@ -4,11 +4,21 @@ public class NodeChr {
 	private int lengthOfChr=8;
 	private int adapter;
 	
+	public NodeChr()
+	{
+		chr=new int[8];
+		adapter=0;
+	}
 	public void setChr(int[] input)
 	{
 		lengthOfChr=chr.length;
 		chr=new int[lengthOfChr];
-		chr=input;
+		int i=0;
+		for(int e:input)
+		{
+			chr[i]=e;
+			i++;
+		}
 	}
 	public void setAdapter()
 	{
--- src/gaAlg.java
+++ src/gaAlg.java
@@ -10,6 +10,11 @@ public class gaAlg {
 	
 	public static void createRandomGeneration(int numOfChr)
 	{
+		/*
+		 * 主要Function
+		 * 產生隨機的子代
+		 * numOfChr = 這一代中染色體各數
+		 */
 		currentChrArray=new NodeChr[numOfChr];
 		for(int i=0;i<numOfChr;i++)
 		{
@@ -19,24 +24,28 @@ public class gaAlg {
 	}
 	private static NodeChr createRandomChr()
 	{
+		/*
+		 * createRandomGeneration的副程式
+		 * 產生隨機的染色體
+		 */
 		int[] gaArray=new int[lengthOfChr];
 		for(int i=0;i<lengthOfChr;i++)
 		{
 			gaArray[i]=gaDataArray[(int) (Math.random()*10)%2];
 		}
-		NodeChr output = null;
+		NodeChr output=new NodeChr();
 		output.setChr(gaArray);
 		return output;
 	}
 	
 	
-	public static void main(String[] arg)
+	/*public static void main(String[] arg)
 	{
 		createRandomGeneration(4);
 		for(int i=0;i<4;i++)
 		{
-			System.out.print(currentChrArray[i].toString());
+			System.out.print(currentChrArray[i].toString()+"\n");
 		}
 			
-	}
+	}*/
 }
